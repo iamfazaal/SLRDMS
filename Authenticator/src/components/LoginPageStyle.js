e9bfb78577bbfb49beb88b896d7600513713efb6
susa98 <48639256+susa98@users.noreply.github.com>
2019-05-06 07:34:16 +0530

    Add new file

---
+++ Authenticator/src/components/LoginPageStyle.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    loginPage: {
+        flex: 1,
+        backgroundColor: 'rgb(255, 67, 255)',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textInput: {
+        height: 20,
+        width: 200,
+        textAlign: 'center',
+        backgroundColor: 'rgb(255, 255, 255)',
+    }
+})
